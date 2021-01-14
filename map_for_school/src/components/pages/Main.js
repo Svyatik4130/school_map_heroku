65d96c2997877dd34a4f9a11c54faf920b9bbd0e
Svyatik4130 <barberbi1@icloud.com>
2021-01-14 18:12:06 +0200

    One more modal on main page was added

--- map_for_school/src/components/pages/Main.js
+++ map_for_school/src/components/pages/Main.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Loader from 'react-loader-spinner'
 import { Container, Row, Col } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap'
+import { useHistory } from "react-router-dom"
 
 import Map from '../layout/Map'
 
 export default function Main() {
+    const [modalShowed, setmodalShowed] = useState(false)
+    const [AdditionalTextForMobileDevices, setAdditionalTextForMobileDevices] = useState()
+
+    let isMainModalShowed = localStorage.getItem("isMainModalShowed");
+    console.log( typeof( Number(isMainModalShowed) ),  (Number(isMainModalShowed) > 2) || (isMainModalShowed !== null))
+    const handleClose = () => {
+        setmodalShowed(true)
+        localStorage.setItem("isMainModalShowed", (isMainModalShowed == null) ? (0) : ((typeof(Number(isMainModalShowed)) === "number") ? (Number(isMainModalShowed) + 1) : (0)));
+    }
+
+    useEffect(() => {   
+        if(isMainModalShowed !== null){
+            if(typeof(Number(isMainModalShowed)) === "number"){
+                if(Number(isMainModalShowed) >= 2){
+                    setmodalShowed(true)
+                }
+            } else {
+                setmodalShowed(true)
+            }
+        }
+
+        if (window.innerWidth < 1000) {
+            setAdditionalTextForMobileDevices(<>
+                <br />
+                <p style={{"color": "#f0ad4e"}} ><strong>📱!If you are on mobile device, you have to tap twice to select the country!📱</strong></p>
+            </>)
+        }
+    }, [])
     return (
         <>
-            {/* <Loader className="pre-loader"
-                type="Bars" color="#FFFFFF" height={80} width={80}
-                timeout={1000} //3 secs
-            // #5b8982
-            /> */}
+            <Modal show={!modalShowed} onHide={() => { handleClose() }} centered>
+                <div className="modal-set-locations">
+                    <Modal.Header closeButton>
+                        <Modal.Title><h4 className="mainModal-notice">Warning!</h4></Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body> <p>This is portfolio, interactive, NodeJS and ReactJS, world map</p> {AdditionalTextForMobileDevices}</Modal.Body>
+                </div>
+            </Modal>
             {/* map and info */}
             <Container fluid>
                 <Row>
