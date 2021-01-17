174fcd022c5a09d4711807e2ac22a7427cc35125
Svyatik4130 <barberbi1@icloud.com>
2021-01-17 23:55:02 +0200

    heroku 404 page implementation

--- map_for_school/src/App.js
+++ map_for_school/src/App.js
@@ -82,7 +82,7 @@ function App() {
               <Route path="/seepoints">
                 <ListPoints />
               </Route>
-              <Route>
+              <Route path='*' exact={true}>
                 <NotFound />
               </Route>
               <Route path="/heroku404">
