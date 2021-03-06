8c228e27a1f6835a3be1a82e0a43e2d44709818e
Svyatik4130 <barberbi1@icloud.com>
2021-03-06 14:18:17 +0200

    footer title change

--- map_for_school/src/components/layout/Footer.js
+++ map_for_school/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="footer-custom-class bg-light text-center text-white text-lg-start">
             <div className="text-center p-2" style={{ "backgroundColor": "#5b8982", "opacity": "0.85"}}>    
-                © 2021 Copyright: Karina, Svyatik, Vovus, Nestya
+                Author: Svyatoslav Leonov
             </div>
         </footer>
     )
